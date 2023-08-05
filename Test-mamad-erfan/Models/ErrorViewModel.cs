76e1cd8ea6dac69ec187b79e78d99981cee5a295
Muhammad <33785330+muhammadganji@users.noreply.github.com>
2023-08-05 11:42:55 +0330

    init

---
+++ Test-mamad-erfan/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Test_mamad_erfan.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
